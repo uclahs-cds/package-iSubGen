3685a1e457b2881c5a45f051e22dd75add089767
Natalie Fox <nfox@mednet.ucla.edu>
2020-02-11 22:45:14 +0000

    update functions
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@74290 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/read.scaling.factors.R
+++ R/read.scaling.factors.R
@@ -1,12 +1,23 @@
 read.scaling.factors <- function(scaling.factor.files.dir, aberration.types) {
 	scaling.factors <- list(center=list(),scale=list());
 	for(aberration.type in aberration.types) {
-		scaling.factors$center[[aberration.type]] <- read.table(
-			paste0(scaling.factor.files.dir,aberration.type,'_gene_recenter.txt')
+		scaling.matrix <- read.table(
+			paste0(scaling.factor.files.dir,aberration.type,'_gene_recenter.txt'),
+			sep='\t',
+			header=TRUE,
+			check.names=FALSE
 			);
-		scaling.factors$scale[[aberration.type]] <- read.table(
-			paste0(scaling.factor.files.dir,aberration.type,'_gene_rescale.txt')
+		scaling.factors$center[[aberration.type]] <- as.numeric(scaling.matrix);
+		names(scaling.factors$center[[aberration.type]]) <- colnames(scaling.matrix);
+
+		scaling.matrix <- read.table(
+			paste0(scaling.factor.files.dir,aberration.type,'_gene_rescale.txt'),
+			sep='\t',
+			header=TRUE,
+			check.names=FALSE
 			);
+		scaling.factors$scale[[aberration.type]] <- as.numeric(scaling.matrix);
+		names(scaling.factors$scale[[aberration.type]]) <- colnames(scaling.matrix);
 	}
 	return(scaling.factors);
 }
--- R/write.scaling.factors.R
+++ R/write.scaling.factors.R
@@ -2,10 +2,12 @@ write.scaling.factors <- function(scaling.factors,scaling.factor.files.dir) {
 	for(x in c('center','scale')) {
 		for(aberration.type in names(scaling.factors[[x]])) {
 			write.table(
-				scaling.factors[[x]][[aberration.type]],
+				t(as.data.frame(scaling.factors[[x]][[aberration.type]])),
 				paste0(scaling.factor.files.dir,aberration.type,'_gene_re',x,'.txt'),
 				quote=FALSE,
-				sep='\t'
+				sep='\t',
+				col.names=TRUE,
+				row.names=FALSE
 				);
 		}
 	}
