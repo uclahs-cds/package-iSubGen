946be7aefafb3336a92d218f8ad8e2ef9d3104c3
Natalie Fox <nfox@mednet.ucla.edu>
2021-04-07 23:09:57 -0400

    function tweak

--- R/apply.scaling.R
+++ R/apply.scaling.R
@@ -29,7 +29,7 @@ apply.scaling <- function(data.matrices, scaling.factors) {
 			}
 
 		# scale each row in the matrix by the corresponding scaling factors
-		for(i in nrow(data.matrices)) {
+		for(i in 1:nrow(data.matrices)) {
 			center.adjustment <- scaling.factors$center[rownames(data.matrices)[i]];
 			scale.adjustment <- 1;
 			if(scaling.factors$scale[rownames(data.matrices)[i]] > 0) {
@@ -41,33 +41,33 @@ apply.scaling <- function(data.matrices, scaling.factors) {
 		# return the scaled single matrix
 		return(data.matrices);
 		}
-	else if (! class(data.matrices) == 'list') {
-		stop('data.matrices needs to be a matrix or a list of matrices');
-		}
-	# if you make it to this point then data.matrices is a list
-	# so check the format of the input and then recurse on each matrix
-
-	# check that scaling.factors are the correct format
-	if (any(sort(names(data.matrices)) != sort(names(scaling.factors)))) {
-		stop('the scaling.factors list needs to have the same names as the data.matrices list');
-		}
-
-	# if you get to this point then data.matrices is a list of matrices
-	for(data.type in names(data.matrices)) {
+	else if (class(data.matrices) == 'list') {
+		# if you make it to this point then data.matrices is a list
+		# so check the format of the input and then recurse on each matrix
 
 		# check that scaling.factors are the correct format
-		if (length(scaling.factors[[data.type]]$center) != nrow(data.matrices[[data.type]])) {
-			stop(paste0('scaling.factors$',data.type,'$center does not match the number of rows in data.matrices$',data.type));
+		if (any(sort(names(data.matrices)) != sort(names(scaling.factors)))) {
+			stop('the scaling.factors list needs to have the same names as the data.matrices list');
 			}
-		if (length(scaling.factors[[data.type]]$scale) != nrow(data.matrices[[data.type]])) {
-			stop(paste0('scaling.factors$',data.type,'$scale does not match the number of rows in data.matrices$',data.type));
-			}
-		
-		# call the function for each data type
-		data.matrices[[data.type]] <- apply.scaling(data.matrices[[data.type]],scaling.factors[[data.type]]);
-		}
 
-	# return the scaled list of matrices
-	return(data.matrices);
+		# if you get to this point then data.matrices is a list of matrices
+		for(data.type in names(data.matrices)) {
+
+			# check that scaling.factors are the correct format
+			if (length(scaling.factors[[data.type]]$center) != nrow(data.matrices[[data.type]])) {
+				stop(paste0('scaling.factors$',data.type,'$center does not match the number of rows in data.matrices$',data.type));
+				}
+			if (length(scaling.factors[[data.type]]$scale) != nrow(data.matrices[[data.type]])) {
+				stop(paste0('scaling.factors$',data.type,'$scale does not match the number of rows in data.matrices$',data.type));
+				}
+		
+			# call the function for each data type
+			data.matrices[[data.type]] <- apply.scaling(data.matrices[[data.type]],scaling.factors[[data.type]]);
+			}
+
+		# return the scaled list of matrices
+		return(data.matrices);
+		}
+	stop('data.matrices needs to be a matrix or a list of matrices');
 	}
 
