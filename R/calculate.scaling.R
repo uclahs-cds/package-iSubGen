bff018c9b22111e9294cf0d1c29cfcabbbbce216
Mao Tian <maotian@mednet.ucla.edu>
2024-08-20 17:07:42 -0700

    update the if condition

--- R/calculate.scaling.R
+++ R/calculate.scaling.R
@@ -5,17 +5,17 @@ calculate.scaling <- function(data.matrices) {
 		return(list(
 			center = apply(data.matrices, 1, mean),
 			scale = apply(data.matrices, 1, sd)
-			));
-		} else if ('list' == class(data.matrices)) {
-			# if there are multiple data types to scale
-			# return the mean and sd of each row for each data matrix
-			scaling.factors <- list();
-			for (data.type in names(data.matrices)) {
-				scaling.factors[[data.type]] <- calculate.scaling(data.matrices[[data.type]]);
-				}
-			return(scaling.factors);
-		} else {
-			# if not a list or a matrix return an error message to let the user know how to correct the input
-			stop('`data.matrices` must be a matrix, a data frame or a list');
+			))
+	if ('list' == class(data.matrices)) {
+		# if there are multiple data types to scale
+		# return the mean and sd of each row for each data matrix
+		scaling.factors <- list();
+		for(data.type in names(data.matrices)) {
+			scaling.factors[[data.type]] <- calculate.scaling(data.matrices[[data.type]]);
+			}
+		return(scaling.factors);
 		}
+	# if `data.matrices` is not a list, a matrix, or a data.frame return an error message to let the user know how to correct the input
+	stop('`data.matrices` must be a matrix, a data frame or a list');
 	}
+}
